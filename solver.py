20cc2b04b1534419b938f70dc4355ba44f230b02
Jeffrey Ching <jcc150@cs.duke.edu>
2025-07-13 14:33:19 -0400

    update solver

--- solver.py
+++ solver.py
@@ -221,47 +221,49 @@ class TestBaseSolver(unittest.TestCase):
 
 
 if __name__ == '__main__':
-#     if len(sys.argv) <= 1:
-#         print ("Usage: test_solver.py [test_file] -op [op]")
-#         print ("\t [test_file]: test file .con; use \'all\' to run all the file under tests/")
-#         print ("\t -partt [0-1]: -partt [0-1]: specifying partition algorithm: 0=sequential,")
-#         print ("\t\t\t1=combinational; if not specified, it tests all the partt ")
-#         print ("\t\t\talgorithms")
-#         print ("\t -appr [0-3]: specifying approaches: 0=hybrid, 1=early-accept, 2=one-shot,")
-#         print ("\t\t\t3=early-reject; if not specified, it tests [0,1,2] approaches.")
-#         print ("\t -time  i : specifying time-out minutes; default i=3 min.")
-#         print ("\t -debug [0-1] : print debug message; default 1 with debug message on")
+    if len(sys.argv) <= 1:
+        print ("Usage: test_solver.py [test_file] -op [op]")
+        print ("\t [test_file]: test file .con; use \'all\' to run all the file under tests/")
+        print ("\t -partt [0-1]: -partt [0-1]: specifying partition algorithm: 0=sequential,")
+        print ("\t\t\t1=combinational; if not specified, it tests all the partt ")
+        print ("\t\t\talgorithms")
+        print ("\t -appr [0-3]: specifying approaches: 0=hybrid, 1=early-accept, 2=one-shot,")
+        print ("\t\t\t3=early-reject; if not specified, it tests [0,1,2] approaches.")
+        print ("\t -time  i : specifying time-out minutes; default i=3 min.")
+        print ("\t -debug [0-1] : print debug message; default 1 with debug message on")
 
-#     else:
-#         file_name = output_file_name("Test1")
-#         # print file_name
-#         # try:
-#         partt = globals.ParttAlg.keys()
-#         appr = globals.Approach.keys()
-#         #appr.pop(globals.EARLY_REJECT_APPROACH)
+    else:
+        file_name = output_file_name("Test1")
+        # print file_name
+        # try:
+        partt = globals.ParttAlg.keys()
+        appr = globals.Approach.keys()
+        #appr.pop(globals.EARLY_REJECT_APPROACH)
         
-#         for i in range(2, len(sys.argv)):
-#             if sys.argv[i] == "-partt":
-#                 partt = [int(sys.argv[i+1])]
-#                 print (partt)
-#             elif sys.argv[i] == "-appr":
-#                 appr = [int(sys.argv[i+1])]
-#             elif sys.argv[i] == "-time":
-#                 globals.STOP_MIN = float(sys.argv[i+1])
-#             elif sys.argv[i] == "-debug":
-#                 globals.DEBUG = int(sys.argv[i+1])
+        for i in range(2, len(sys.argv)):
+            print(sys.argv[i])
+            if sys.argv[i] == "-partt":
+                partt = [int(sys.argv[i+1])]
+                print (partt)
+            elif sys.argv[i] == "-appr":
+                appr = [int(sys.argv[i+1])]
+            elif sys.argv[i] == "-time":
+                globals.STOP_MIN = float(sys.argv[i+1])
+            elif sys.argv[i] == "-debug":
+                globals.DEBUG = int(sys.argv[i+1])
 
-#         file_test = sys.argv[1]
-#         if sys.argv[1] == "all":
-#             file_test = [TEST_DIR + file for file in os.listdir(TEST_DIR)]
-#             for test_file_name in file_test:
-#                 test_file(test_file_name, partt, appr)
-#         else:
-#             test_file(file_test, partt, appr)
+        file_test = sys.argv[1]
+        # print(file_test)
+        # if sys.argv[1] == "all":
+        #     file_test = [TEST_DIR + file for file in os.listdir(TEST_DIR)]
+        #     for test_file_name in file_test:
+        #         test_file(test_file_name, partt, appr)
+        # else:
+        #     test_file(file_test, partt, appr)
 
-#         # save result:
-#         if files:
-#             store_result_file(file_name, files, number, partt_alg, perform)
+        # # save result:
+        # if files:
+        #     store_result_file(file_name, files, number, partt_alg, perform)
 
         # if sys.argv[1] == 'all':
         #     plot_line_chart(file_name + plt_ext, partt, appr, number, partt_alg, perform)
